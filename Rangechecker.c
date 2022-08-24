b4e24386b070e395757725f12735ede0530a64eb
deepasn08 <deepareddy997@gmail.com>
2022-08-25 00:50:44 +0530

    v3

--- Rangechecker.c
+++ Rangechecker.c
@@ -1,49 +1,45 @@
 #include "Rangechecker.h"
 
-void swap(int* x, int* y){
-    int temp = *x;
-    *x = *y;
-    *y = temp;
-}
-
-int *sortinputarray(int *list, int size){
-int i, j, min_idx;
-
-    // One by one move boundary of unsorted subarray
-    for (i = 0; i < size - 1; i++) {
-        // Find the minimum element in unsorted array
-        min_idx = i;
-        for (j = i + 1; j < size; j++)
-            if (list[j] < list[min_idx])
-                min_idx = j;
-        swap(&list[min_idx], &list[i]);
+int * sortinputarray(int *list, int size){
+    int i, j, temp;
+    for (i = 0; i < size - 1; ++i)
+    {
+        for (j = 0; j < size - 1 - i; ++j )
+        {
+            if (list[j] > list[j+1])
+            {
+                temp = list[j+1];
+                list[j+1] = list[j];
+                list[j] = temp;
+            }
+        }
     }
 
-    return &list;
+    return list;    
 }
 
 
-int *checkrangesofsortedarray(int *sortedlist, int size){
-int no_of_Ranges = 0;
-	
-struct range rangelist[10]; 
-	
-rangelist[no_of_Ranges].startindex = sortedlist[0]; 
-	
-	for(int index = 0; index < size-1; index++) {
-		if((sortedlist[index] == sortedlist[index+1]) || (sortedlist[index]+1 == sortedlist[index+1]))
-		{
-			rangelist[no_of_Ranges].endindex = sortedlist[index+1];
-			++rangelist[no_of_Ranges].rangecount; 
-		}
-		else
-		{	
-			++no_of_Ranges;
-			rangelist[no_of_Ranges].startindex = sortedlist[index+1];
-		}
-	}
+int checkrangesofsortedarray(struct range * rangelist, int *sortedlist, int size){
+     
+    int no_of_Ranges = 0;
+    rangelist[no_of_Ranges].startindex = sortedlist[0];
+    rangelist[no_of_Ranges].rangecount = 1;
+    for (int index = 0; index < size; index++)
+    {
+        if ((sortedlist[index] == sortedlist[index + 1]) || (sortedlist[index] + 1 == sortedlist[index + 1]))
+        {
+            rangelist[no_of_Ranges].endindex = sortedlist[index + 1];
+            rangelist[no_of_Ranges].rangecount = rangelist[no_of_Ranges].rangecount+1;
+        }
+        else
+        {
+            ++no_of_Ranges;
+            rangelist[no_of_Ranges].rangecount = 1;
+            rangelist[no_of_Ranges].startindex = sortedlist[index + 1];
+        }
+    }
 
-    return &rangelist;
+    return 0;
 }
 
 void printrangeonconsole(struct range *rangelist, int size){
@@ -54,13 +50,13 @@ void printrangeonconsole(struct range *rangelist, int size){
 }
 
 char *getrangeincsvformat(struct range *rangelist, int index){
-char s1[6],s2[6],s3[6],s4[6];
-sprintf(s1, "%d", (rangelist[index].startindex));
-sprintf(s2, "%d", (rangelist[index].endindex));
-sprintf(s3, "%d", (rangelist[index].rangecount));
-char string1 = strcat(s1,"-");
-char string2 = strcat(string1, s2);
-char string3 = strcat(string2, ", ");
-char string4 = strcat(string3, s3);
-return string4;
+    char s1[6],s2[6],s3[6],s4[6];
+    sprintf(s1, "%d", (rangelist[index].startindex));
+    sprintf(s2, "%d", (rangelist[index].endindex));
+    sprintf(s3, "%d", (rangelist[index].rangecount));
+    char string1 = strcat(s1,"-");
+    char string2 = strcat(string1, s2);
+    char string3 = strcat(string2, ", ");
+    char string4 = strcat(string3, s3);
+    return string4;
 }
--- Rangechecker.h
+++ Rangechecker.h
@@ -8,10 +8,13 @@ struct range
     int rangecount;
 }range;
 
-void sortgivenlist(int *Range, int noOfElements, int received_position) ;
+//void sortgivenlist(int *Range, int noOfElements, int received_position) ;
 void swap(int* x, int* y);
 int *sortinputarray(int *list, int size);
-int *checkrangesofsortedarray(int *sortedlist, int size);
+int checkrangesofsortedarray(struct range * rangelist, int *sortedlist, int size);
 void printrangeonconsole(struct range *rangelist, int size);
 char *getrangeincsvformat(struct range *rangelist, int index);
+
+int TestSortedarray();
+int TestOutputrange();
 
